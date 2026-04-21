24ea2d6816d341f16f5be05d0a1c949816b41b17
aqys <mikkelmartinlarsen@gmail.com>
2026-04-21 09:15:29 +0200

    Cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,31 +2,17 @@
   import RanklistTable from '../components/RanklistTable.vue'
   import MatchList from '../components/MatchList.vue'
 
-  type Match = {
-    id: number
-    tidspunkt: string
-    vinder: string
-    vinderScore: number
-    taber: string
-    taberScore: number
-  }
-
-  const matches: Match[] = [
-    { id: 1, tidspunkt: '20-04-2026 14:32', vinder: 'Anders', vinderScore: 3, taber: 'Mads', taberScore: 1 },
-    { id: 2, tidspunkt: '20-04-2026 13:10', vinder: 'Sofie', vinderScore: 3, taber: 'Anders', taberScore: 2 },
-    { id: 3, tidspunkt: '19-04-2026 18:45', vinder: 'Mads', vinderScore: 3, taber: 'Sofie', taberScore: 0 }
-  ]
 </script>
 
 <template>
   <main class="grid">
     <section class="panel">
-      <h2>Rangliste</h2>
+      <h2>Rankliste</h2>
       <RanklistTable />
     </section>
 
     <section class="panel">
-      <MatchList :matches="matches" />
+      <MatchList />
     </section>
   </main>
 </template>
